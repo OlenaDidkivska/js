f21434f6091e7382e29295c051f68e73b347d1d8
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-06-04 22:46:13 +0300

    add js

--- js/lessons-one.js
+++ js/lessons-one.js
@@ -1,10 +1,10 @@
+"use strict";
 const age = 10;
 const totalPrice = 210.64;
-const userName = 'FinePoline';
-const massage = 'Приветик';
+const userName = "FinePoline";
+const massage = "Приветик";
 const isOpen = true;
 const shouldConfirm = false;
 
-console.log(totalPrice);
-
- 
+console.log(5);
+console.log("Ціна", totalPrice, "грн");
