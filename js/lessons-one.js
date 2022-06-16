054590efaecbbc63da0999e0bc97fb13916b2da6
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-06-16 22:27:15 +0300

    add

--- js/lessons-one.js
+++ js/lessons-one.js
@@ -1,10 +1,24 @@
-"use strict";
-const age = 10;
-const totalPrice = 210.64;
-const userName = "FinePoline";
-const massage = "Приветик";
-const isOpen = true;
-const shouldConfirm = false;
+// const age = 10;
+// const totalPrice = 210.64;
+// const userName = "FinePoline";
+// const massage = "Приветик";
+// const isOpen = true;
+// const shouldConfirm = false;
 
-console.log(5);
-console.log("Ціна", totalPrice, "грн");
+// console.log(5);
+// console.log("Ціна", totalPrice, "грн");
+
+// const type = typeof true;
+
+// console.log(type);
+
+// console.log("Before");
+// alert("Hello MOTO");
+// console.log("After");
+
+// const shouldRenew = confirm("Желаете продлить подписку?");
+// console.log(shouldRenew);
+let quantity = prompt("Сколько Вам лет?");
+quantity = Number(quantity);
+console.log(quantity, "лет");
+console.log(typeof quantity);
